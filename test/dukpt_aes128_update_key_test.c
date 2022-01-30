f997fc9f34fcdac961bfb2279c57e46ff94cc62d
Leon Lynch <lynch.leon@gmail.com>
2022-01-30 15:25:54 +0100

    Remove TDES update key testing for AES DUKPT
    Although TDES update keys are possible for AES DUKPT, they are rarely
    supported by HSMs.

--- test/dukpt_aes128_update_key_test.c
+++ test/dukpt_aes128_update_key_test.c
@@ -32,10 +32,10 @@ static const uint8_t ikid[] = { 0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56 }
 static const uint8_t update_key_aes128_verify[] = { 0x9A, 0x97, 0x70, 0xAE, 0xE1, 0xAC, 0xD1, 0xB1, 0x34, 0x73, 0xD0, 0x46, 0x3A, 0x18, 0x83, 0xB9 };
 
 // ANSI X9.24-3:2017 Supplement Test Vectors for AES-128 BDK (update key for TDES2; middle of page 24)
-static const uint8_t update_key_tdes2_verify[] = { 0x47, 0x44, 0xA5, 0xEC, 0xBC, 0x62, 0xB5, 0xC4, 0xBB, 0x76, 0xFB, 0xEA, 0xE1, 0xE2, 0x44, 0xA3 };
+//static const uint8_t update_key_tdes2_verify[] = { 0x47, 0x44, 0xA5, 0xEC, 0xBC, 0x62, 0xB5, 0xC4, 0xBB, 0x76, 0xFB, 0xEA, 0xE1, 0xE2, 0x44, 0xA3 };
 
 // ANSI X9.24-3:2017 Supplement Test Vectors for AES-128 BDK (update key for TDES3; top of page 30)
-static const uint8_t update_key_tdes3_verify[] = { 0xAF, 0x82, 0xBE, 0x85, 0x33, 0xCF, 0xCA, 0x52, 0x6D, 0xA7, 0x17, 0x08, 0x66, 0x7A, 0xD0, 0xBB, 0xC7, 0xA7, 0x51, 0x75, 0x04, 0xC7, 0x8C, 0x8A };
+//static const uint8_t update_key_tdes3_verify[] = { 0xAF, 0x82, 0xBE, 0x85, 0x33, 0xCF, 0xCA, 0x52, 0x6D, 0xA7, 0x17, 0x08, 0x66, 0x7A, 0xD0, 0xBB, 0xC7, 0xA7, 0x51, 0x75, 0x04, 0xC7, 0x8C, 0x8A };
 
 static void print_buf(const char* buf_name, const void* buf, size_t length)
 {
@@ -51,9 +51,10 @@ int main(void)
 {
 	int r;
 	uint8_t update_key_aes128[DUKPT_AES_KEY_LEN(AES128)];
-	uint8_t update_key_tdes2[DUKPT_AES_KEY_LEN(TDES2)];
-	uint8_t update_key_tdes3[DUKPT_AES_KEY_LEN(TDES3)];
+	//uint8_t update_key_tdes2[DUKPT_AES_KEY_LEN(TDES2)];
+	//uint8_t update_key_tdes3[DUKPT_AES_KEY_LEN(TDES3)];
 
+	// Test AES-128 update key
 	r = dukpt_aes_derive_update_key(ik, sizeof(ik), ikid, DUKPT_AES_KEY_TYPE_AES128, update_key_aes128);
 	if (r) {
 		fprintf(stderr, "dukpt_aes_derive_update_key() failed; r=%d\n", r);
@@ -67,6 +68,8 @@ int main(void)
 		goto exit;
 	}
 
+	/*
+	// Test TDES2 update key
 	r = dukpt_aes_derive_update_key(ik, sizeof(ik), ikid, DUKPT_AES_KEY_TYPE_TDES2, update_key_tdes2);
 	if (r) {
 		fprintf(stderr, "dukpt_aes_derive_update_key() failed; r=%d\n", r);
@@ -80,6 +83,7 @@ int main(void)
 		goto exit;
 	}
 
+	// Test TDES3 update key
 	r = dukpt_aes_derive_update_key(ik, sizeof(ik), ikid, DUKPT_AES_KEY_TYPE_TDES3, update_key_tdes3);
 	if (r) {
 		fprintf(stderr, "dukpt_aes_derive_update_key() failed; r=%d\n", r);
@@ -92,6 +96,7 @@ int main(void)
 		r = 1;
 		goto exit;
 	}
+	*/
 
 	printf("All tests passed.\n");
 	r = 0;
--- test/dukpt_aes256_update_key_test.c
+++ test/dukpt_aes256_update_key_test.c
@@ -35,6 +35,7 @@ static const uint8_t ikid[] = { 0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56 }
 static const uint8_t update_key_aes128_verify[] = {
 	0x90, 0xE5, 0x4E, 0x4A, 0x70, 0x16, 0x0C, 0x7E, 0x08, 0x5C, 0x09, 0xD2, 0xB2, 0x41, 0xD3, 0x43,
 };
+// ANSI X9.24-3:2017 Supplement Test Vectors for AES-256 BDK (update key for AES-256; bottom of page 18)
 static const uint8_t update_key_aes256_verify[] = {
 	0xAE, 0xFB, 0x21, 0x0C, 0x13, 0x62, 0x78, 0xA1, 0x27, 0x9F, 0x7C, 0x88, 0x15, 0xF4, 0x46, 0xDB,
 	0x8E, 0xBE, 0x2A, 0xA9, 0x10, 0xB1, 0x57, 0xAA, 0x4E, 0x64, 0x84, 0xD8, 0xDE, 0x9C, 0x48, 0x07,
@@ -56,6 +57,7 @@ int main(void)
 	uint8_t update_key_aes128[DUKPT_AES_KEY_LEN(AES128)];
 	uint8_t update_key_aes256[DUKPT_AES_KEY_LEN(AES256)];
 
+	// Test AES-128 update key
 	r = dukpt_aes_derive_update_key(ik, sizeof(ik), ikid, DUKPT_AES_KEY_TYPE_AES128, update_key_aes128);
 	if (r) {
 		fprintf(stderr, "dukpt_aes_derive_update_key() failed; r=%d\n", r);
@@ -69,6 +71,7 @@ int main(void)
 		goto exit;
 	}
 
+	// Test AES-256 update key
 	r = dukpt_aes_derive_update_key(ik, sizeof(ik), ikid, DUKPT_AES_KEY_TYPE_AES256, update_key_aes256);
 	if (r) {
 		fprintf(stderr, "dukpt_aes_derive_update_key() failed; r=%d\n", r);
