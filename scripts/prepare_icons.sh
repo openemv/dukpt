a89049461d1997d354087cd20830e9083ed54e28
Leon Lynch <lynch.leon@gmail.com>
2023-09-09 18:57:05 +0200

    Update icons for dukpt-ui
    The prepare_icons.sh script has been updated to offset the original
    image such that the card appears in the center of the icon. The script
    has also been updated to use png2icns, which is provided by
    https://sourceforge.net/projects/icns/, to generate an icon for MacOS.

--- scripts/prepare_icons.sh
+++ scripts/prepare_icons.sh
@@ -7,6 +7,9 @@ set -x
 # delegate processing of SVGs to various different backends that may have
 # different behaviour regarding the alpha channel, it's best to use a PNG icon
 # with the correct alpha channel as the input icon.
+#
+# NOTE: this script also depends on png2icns (see
+# https://sourceforge.net/projects/icns/)
 
 function help {
 	printf "Usage: %s [icons-dir] [input-icon]\n" "$(basename "$0")"
@@ -25,12 +28,18 @@ if [ ! -f "${input_icon}" ]; then
 	exit 1
 fi
 
-# Trim the input icon before creating the various resized outputs
-magick "${input_icon}" -trim "${icons_dir}/openemv_dukpt_trimmed.png"
+# Trim and center the input icon before creating the various resized outputs
+# NOTE: 57px offset ensures that the card image is in the center
+magick "${input_icon}" -trim -resize 1024x1024 -background none -gravity center -extent 1024x1024+0-57 "${icons_dir}/openemv_dukpt_1024x1024.png"
 
 # Output various icon sizes for use in the menu, app window, taskbar and installer
-magick "${icons_dir}/openemv_dukpt_trimmed.png" -resize 512x512 -background none -gravity center -extent 512x512 "${icons_dir}/openemv_dukpt_512x512.png"
-magick "${icons_dir}/openemv_dukpt_trimmed.png" -resize 256x256 -background none -gravity center -extent 256x256 "${icons_dir}/openemv_dukpt_256x256.png"
+magick "${icons_dir}/openemv_dukpt_1024x1024.png" -resize 512x512 -background none -gravity center -extent 512x512 "${icons_dir}/openemv_dukpt_512x512.png"
+magick "${icons_dir}/openemv_dukpt_1024x1024.png" -resize 256x256 -background none -gravity center -extent 256x256 "${icons_dir}/openemv_dukpt_256x256.png"
 magick convert "${icons_dir}/openemv_dukpt_256x256.png" "${icons_dir}/openemv_dukpt.ico"
-magick "${icons_dir}/openemv_dukpt_trimmed.png" -resize 150x57 -background white -gravity center -extent 150x57 "${icons_dir}/openemv_dukpt_150x57.png"
+
+# Output icon for MacOS
+png2icns "${icons_dir}/openemv_dukpt.icns" "${icons_dir}/openemv_dukpt_1024x1024.png"
+
+# Trim and relocate the input icon for Windows NSIS installer (150x57 geometry)
+magick "${input_icon}" -trim -resize 150x53 -background white -gravity center -extent 150x53 - | magick - -resize 150x57 -background white -gravity south -extent 150x57 "${icons_dir}/openemv_dukpt_150x57.png"
 magick convert "${icons_dir}/openemv_dukpt_150x57.png" "BMP3:${icons_dir}/openemv_dukpt.bmp"
