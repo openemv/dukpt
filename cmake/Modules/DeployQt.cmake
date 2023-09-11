fcc52f3ee4404fb71c24763ec0766d461d88005c
Leon Lynch <lynch.leon@gmail.com>
2023-09-11 20:37:48 +0200

    Update packaging for Qt6 deployment
    * Update DeployQt.cmake to rely on QT_VERSION_MAJOR for the preferred Qt
      version
    * Update DeployQt.cmake to use different windeployqt options for
      different versions of Qt because these options differ between Qt6 and
      Qt5
    * Let add_custom_command() specify VERBATIM as recommended by the CMake
      documentation. This ensures that variables are expanded correctly and
      in a platform independent way.

--- cmake/Modules/DeployQt.cmake
+++ cmake/Modules/DeployQt.cmake
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2022 Leon Lynch
+# Copyright (c) 2022, 2023 Leon Lynch
 #
 # This file is licensed under the terms of the LGPL v2.1 license.
 # See LICENSE file.
@@ -10,29 +10,61 @@
 #
 ##############################################################################
 
-find_package(Qt5Core REQUIRED)
-
-# Find Qt installation
-get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
-get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-
-find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
-if(APPLE AND NOT MACDEPLOYQT_EXECUTABLE)
-	message(FATAL_ERROR "macdeployqt not found")
+# Parent context must specify Qt major version using QT_VERSION_MAJOR.
+# This variable can either be set explicitly or be generated when finding the
+# available Qt versions, for example: find_package(QT NAMES Qt6 Qt5)
+if(NOT QT_VERSION_MAJOR)
+	message(FATAL_ERROR "QT_VERSION_MAJOR not set")
 endif()
 
-find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
-if(WIN32 AND NOT WINDEPLOYQT_EXECUTABLE)
-	message(FATAL_ERROR "windeployqt not found")
+# Find Qt installation
+find_package(Qt${QT_VERSION_MAJOR}Core REQUIRED)
+if(QT_VERSION_MAJOR EQUAL 6)
+	get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
+elseif(QT_VERSION_MAJOR EQUAL 5)
+	get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+else()
+	message(FATAL_ERROR "Deployment of Qt${QT_VERSION_MAJOR} not supported")
+endif()
+get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+
+# Find Qt deployment tool for MacOS
+if(APPLE)
+	find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
+	if(MACDEPLOYQT_EXECUTABLE)
+		message(STATUS "Found macdeployqt: ${MACDEPLOYQT_EXECUTABLE}")
+	else()
+		message(FATAL_ERROR "macdeployqt not found")
+	endif()
+endif()
+
+# Find Qt deployment tool for Windows
+if(WIN32)
+	find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+	if(WINDEPLOYQT_EXECUTABLE)
+		message(STATUS "Found windeployqt: ${WINDEPLOYQT_EXECUTABLE}")
+	else()
+		message(FATAL_ERROR "windeployqt not found")
+	endif()
+
+	# Set options used to exclude modules for different versions of Qt
+	if(QT_VERSION_MAJOR EQUAL 6)
+		set(WINDEPLOYQT_EXCLUDE_OPTS "--no-translations" "--no-opengl-sw")
+	elseif(QT_VERSION_MAJOR EQUAL 5)
+		set(WINDEPLOYQT_EXCLUDE_OPTS "--no-translations" "--no-webkit2" "--no-angle" "--no-opengl-sw")
+	else()
+		message(FATAL_ERROR "Deployment of Qt${QT_VERSION_MAJOR} not supported")
+	endif()
 endif()
 
 function(macdeployqt target component)
 	add_custom_command(TARGET ${target} POST_BUILD
 		COMMAND "${MACDEPLOYQT_EXECUTABLE}"
-			\"$<TARGET_BUNDLE_DIR:${target}>\"
+			$<TARGET_BUNDLE_DIR:${target}>
 			-verbose=1
 			-always-overwrite
 		COMMENT "Deploying Qt..."
+		VERBATIM
 	)
 endfunction()
 
@@ -44,13 +76,11 @@ function(windeployqt target component)
 			env PATH="${_qt_bin_dir}$<SEMICOLON>%PATH%" "${WINDEPLOYQT_EXECUTABLE}"
 			--verbose 3
 			--compiler-runtime
-			--no-translations
-			--no-webkit2
-			--no-angle
-			--no-opengl-sw
+			${WINDEPLOYQT_EXCLUDE_OPTS}
 			--dir "${CMAKE_CURRENT_BINARY_DIR}/qt-bin/"
 			$<TARGET_FILE:${target}>
 		COMMENT "Deploying Qt..."
+		VERBATIM
 	)
 
 	# Install Qt to the appropriate CMake component
